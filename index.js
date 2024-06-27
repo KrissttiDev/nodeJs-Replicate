084ea6b68b9035392fe6ceb066cdc03f3d356567
KrissttiDev <krisstti.sharing@gmail.com>
2024-06-27 11:50:56 -0400

    chat model

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ import express  from 'express';
 import Replicate from 'replicate';
 
 const app = express();
+
+const replicate = new Replicate({
+    auth: process.env.REPLICATE_API_KEY,
+});
 //midleware  convierte lo exprees en json si no no funciona
 app.use(express.json());
+
+
 
 
 app.post('/generate', async(req, res)=>{
@@ -13,9 +19,7 @@ app.post('/generate', async(req, res)=>{
         return res.status(400).json({error:"Prompt is required"});
     }
 
-    const replicate = new Replicate({
-        auth: process.env.REPLICATE_API_KEY,
-    });
+    
     const input = {
         cfg:4.5,
         prompt,
@@ -30,6 +34,31 @@ app.post('/generate', async(req, res)=>{
     
     res.json(output);
 });
+
+app.post('/chat', async(req, res)=>{
+    const {prompt}= req.body;
+    if (!prompt) {
+        return res.status(400).json({error:"Prompt is required"});
+    }
+    const input = {
+        top_k: 0,
+        top_p: 0.9,
+        prompt,
+        max_tokens: 512,
+        min_tokens: 0,
+        temperature: 0.6,
+        length_penalty: 1,
+        prompt_template: "{prompt}",
+        presence_penalty: 1.15,
+        log_performance_metrics: false
+      };
+      
+      for await (const event of replicate.stream("meta/meta-llama-3-8b", { input })) {
+        //process.stdout.write(event.toString());
+        res.write(event.toString());
+      };
+      res.end();
+})
 
 app.listen(3000, ()=>{
     console.log('Server running on port 3000');
